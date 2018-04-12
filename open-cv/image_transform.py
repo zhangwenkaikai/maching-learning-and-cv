8932a99a65ebee2849577f5e3b4c95444d134305
骑马小猫 <1435130236@qq.com>
2018-04-12 10:38:44 +0800

    '完成验证'

--- open-cv/image_transform.py
+++ open-cv/image_transform.py
@@ -152,7 +152,6 @@ def detect_heros():
 
 def detect_face_in_video():
     capture = cv2.VideoCapture(0)
-
     success, frame = capture.read()
 
     face_xml = "haarcascade_frontalface_alt.xml"
@@ -160,18 +159,14 @@ def detect_face_in_video():
     face_cascade = cv2.CascadeClassifier(face_xml)
 
     while success and cv2.waitKey(1) == -1:
-
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
         faces = face_cascade.detectMultiScale(gray, 1.2, 2)
-
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
-
-        cv2.imshow("ss",frame)
-
+        cv2.imshow("ss", frame)
         success, frame = capture.read()
 
     capture.release()
+
 
 detect_face_in_video()
